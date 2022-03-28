55d74ef1903231974558a8212b4c95617eee0384
BrenoAndrade03 <brenopereira263@gmail.com>
2022-03-28 16:42:27 -0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import favoriteFruit from "./banana.jpg"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class State extends React.Component{
+
+  state ={
+    nome: 'Breno',
+    idade: 18,
+    comidaFavorita: 'Escondidinho de frango',
+    musicas:["Incendeia", "Zombie", "Oceans","Tell me milet"],
+  }
+
+  render(){
+    return(
+      <div>
+        <h1> Eaí, meu nome é {this.state.nome}.</h1>
+        <h2> Eu tenho {this.state.idade},</h2>
+        <h3>Gosto de {this.state.comidaFavorita}.</h3>
+        <h3>Eu amo muito dessas musicas: </h3>
+        
+        <ol type="I">
+          <li> {this.state.musicas[0]} </li>
+          <li> {this.state.musicas[1]} </li>
+          <li> {this.state.musicas[2]} </li>
+          <li> {this.state.musicas[3]} </li>
+        </ol>
+        <p>E essa é a minha fruta favorita.</p>
+        <img src= {favoriteFruit} alt="imagem de uma Banana" />
+      </div>
+    )
+  }
 }
-
-export default App;
